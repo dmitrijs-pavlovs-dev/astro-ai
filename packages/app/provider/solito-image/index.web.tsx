76f8218234657417d2418872f5fae63375c286c7
Dmitrijs Pavlovs <dmitirjs@doit.com>
2025-03-12 19:08:26 +0000

    test

--- packages/app/provider/solito-image/index.web.tsx
+++ packages/app/provider/solito-image/index.web.tsx
@@ -1,7 +1,7 @@
 import { SolitoImageProvider as SolitoImageProviderOG } from 'solito/image'
 
 const imageURL = process.env.NEXT_PUBLIC_APP_URL as `http:${string}` | `https:${string}`
-
+console.log(process.env.NEXT_PUBLIC_APP_URL, 'NEXT_PUBLIC_APP_URL')
 export const SolitoImageProvider = ({
   children,
 }: {
